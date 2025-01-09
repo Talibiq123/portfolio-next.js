869e1da81eae3abbc8652a8a9f61ba3953a27254
Talibiq123 <talibsaeed90@gmail.com>
2025-01-09 19:41:25 +0530

    contact

---
+++ components/Contact.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div id="contact" className="min-h-screen w-full px-[12%] py-10 scroll-mt-20">
+      <h4 className="text-center mb-2 text-lg font-Ovo">Connnect with me</h4>
+      <h2 className="text-4xl font-extrabold text-gray-800 text-center mb-12">
+        Get in touch
+      </h2>
+
+      <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
+        I'd love to hear from you! If you have any questions, comments, or feedback, please use the form below
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
