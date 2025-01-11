e6bb5cd6340cf1e40861d5293a488dd653089687
Talibiq123 <talibsaeed90@gmail.com>
2025-01-11 18:45:37 +0530

    form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from "next/image";
+import { assets } from "@/assets/assets";
 
 const Contact = () => {
   return (
@@ -14,12 +16,12 @@ const Contact = () => {
 
       <form className=' max-w-2xl mx-auto'>
         <div className=' grid grid-cols-auto gap-6 mt-10 mb-8'>
-          <input type="text" placeholder='Enter your name' required />
-          <input type="email" placeholder='Enter your email' required />
+          <input type="text" placeholder='Enter your name' required className=' flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
+          <input type="email" placeholder='Enter your email' required className='  flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
         </div>
-        <textarea rows='6' placeholder='Enter your message' required></textarea>
+        <textarea rows='6' placeholder='Enter your message' required className=' w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'></textarea>
 
-        <button type='submit'>Submit Now</button>
+        <button type='submit' className=' py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'>Submit Now <Image src={assets.ri} /> </button>
       </form>
     </div>
   );
