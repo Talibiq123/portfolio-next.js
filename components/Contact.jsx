5809176cb62df53dce3f9bfb1a38cc5629a341f1
Talibiq123 <talibsaeed90@gmail.com>
2025-01-12 11:59:11 +0530

    form submission done

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
 import { assets } from "@/assets/assets";
 
@@ -43,10 +43,10 @@ const Contact = () => {
 
       <form onSubmit={onSubmit} className=' max-w-2xl mx-auto'>
         <div className=' grid grid-cols-auto gap-6 mt-10 mb-8'>
-          <input type="text" placeholder='Enter your name' required className=' flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
-          <input type="email" placeholder='Enter your email' required className='  flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' />
+          <input type="text" placeholder='Enter your name' required className=' flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='name' />
+          <input type="email" placeholder='Enter your email' required className='  flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white' name='email' />
         </div>
-        <textarea rows='6' placeholder='Enter your message' required className=' w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'></textarea>
+        <textarea rows='6' placeholder='Enter your message' required className=' w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6' name='message'></textarea>
 
         <button type='submit' className=' py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'>Submit Now <Image src={assets.right_arrow_white} alt='' className=' w-4' /> </button>
       
